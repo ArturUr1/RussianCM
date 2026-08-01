83b177035c5e7df57df70b56534421c0eae2e814
Maiji <51055875+TheMaiji@users.noreply.github.com>
2026-08-01 21:43:28 +0300

    test: reference Yautja youngblood call command

--- Content.IntegrationTests/_CMU14/Yautja/YautjaClanAdminEntryTest.cs
+++ Content.IntegrationTests/_CMU14/Yautja/YautjaClanAdminEntryTest.cs
@@ -28,7 +28,7 @@ public sealed class YautjaClanAdminEntryTest
             typeof(YautjaClanAdminCommand),
             typeof(YautjaClanInfoCommand),
             typeof(YautjaPredatorAdminEditorCommand),
-            typeof(YautjaYoungbloodAdminCommand),
+            typeof(YautjaYoungbloodCallCommand),
             typeof(YautjaClanSetMemberCommand),
             typeof(YautjaClanCreateCommand),
             typeof(YautjaClanWhitelistCommand),
