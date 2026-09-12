dd2f9e42b23c41e632be39401ee0c57ce523482d
Advancer <113517899+flex5hybrid@users.noreply.github.com>
2026-09-13 02:50:20 +0300

    Keep radio implant diff focused

--- Content.Server/Implants/RadioImplantSystem.cs
+++ Content.Server/Implants/RadioImplantSystem.cs
@@ -14,6 +14,9 @@ public sealed partial class RadioImplantSystem : EntitySystem
         SubscribeLocalEvent<RadioImplantComponent, ImplantRemovedEvent>(OnImplantRemoved);
     }
 
+    /// <summary>
+    /// If implanted with a radio implant, installs the necessary intrinsic radio components
+    /// </summary>
     private void OnImplantImplanted(Entity<RadioImplantComponent> ent, ref ImplantImplantedEvent args)
     {
         var activeRadio = EnsureComp<ActiveRadioComponent>(args.Implanted);
@@ -35,25 +38,32 @@ public sealed partial class RadioImplantSystem : EntitySystem
         Dirty(args.Implanted, intrinsicRadioTransmitter);
     }
 
+    /// <summary>
+    /// Removes intrinsic radio components once the Radio Implant is removed
+    /// </summary>
     private void OnImplantRemoved(Entity<RadioImplantComponent> ent, ref ImplantRemovedEvent args)
     {
         if (TryComp<ActiveRadioComponent>(args.Implanted, out var activeRadioComponent))
         {
             foreach (var channel in ent.Comp.ActiveAddedChannels)
+            {
                 activeRadioComponent.Channels.Remove(channel);
-
+            }
             ent.Comp.ActiveAddedChannels.Clear();
 
             if (activeRadioComponent.Channels.Count == 0)
+            {
                 RemCompDeferred<ActiveRadioComponent>(args.Implanted);
+            }
         }
 
         if (!TryComp<IntrinsicRadioTransmitterComponent>(args.Implanted, out var radioTransmitterComponent))
             return;
 
         foreach (var channel in ent.Comp.TransmitterAddedChannels)
+        {
             radioTransmitterComponent.Channels.Remove(channel);
-
+        }
         ent.Comp.TransmitterAddedChannels.Clear();
 
         if (radioTransmitterComponent.Channels.Count == 0 || activeRadioComponent?.Channels.Count == 0)
