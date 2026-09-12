432b6a695116e6672abf0edea31a6c9e3968f796
Advancer <113517899+flex5hybrid@users.noreply.github.com>
2026-09-13 02:49:19 +0300

    Sync intrinsic radio channels to clients

--- Content.Shared/Radio/Components/IntrinsicRadioTransmitterComponent.cs
+++ Content.Shared/Radio/Components/IntrinsicRadioTransmitterComponent.cs
@@ -4,13 +4,9 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Radio.Components;
 
-/// <summary>
-///     This component allows an entity to directly translate spoken text into radio messages (effectively an intrinsic
-///     radio headset).
-/// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class IntrinsicRadioTransmitterComponent : Component
 {
-    [DataField]
+    [DataField, AutoNetworkedField]
     public HashSet<ProtoId<RadioChannelPrototype>> Channels = new() { SharedChatSystem.CommonChannel };
 }
