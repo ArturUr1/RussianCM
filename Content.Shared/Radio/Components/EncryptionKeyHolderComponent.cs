3a4a95a396b84df8d0beed3d2ff27f40d93a437b
Advancer <113517899+flex5hybrid@users.noreply.github.com>
2026-09-13 02:49:14 +0300

    Sync headset radio channels to clients

--- Content.Shared/Radio/Components/EncryptionKeyHolderComponent.cs
+++ Content.Shared/Radio/Components/EncryptionKeyHolderComponent.cs
@@ -10,18 +10,12 @@ namespace Content.Shared.Radio.Components;
 /// <summary>
 ///     This component is by entities that can contain encryption keys
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class EncryptionKeyHolderComponent : Component
 {
-    /// <summary>
-    ///     Whether or not encryption keys can be removed from the headset.
-    /// </summary>
     [DataField]
     public bool KeysUnlocked = true;
 
-    /// <summary>
-    ///     The tool required to extract the encryption keys from the headset.
-    /// </summary>
     [DataField]
     public ProtoId<ToolQualityPrototype> KeysExtractionMethod = "Screwing";
 
@@ -38,20 +32,12 @@ public sealed partial class EncryptionKeyHolderComponent : Component
     public Container KeyContainer = default!;
     public const string KeyContainerName = "key_slots";
 
-    /// <summary>
-    ///     Combined set of radio channels provided by all contained keys.
-    /// </summary>
-    [ViewVariables]
+    [ViewVariables, AutoNetworkedField]
     public HashSet<ProtoId<RadioChannelPrototype>> Channels = new();
 
-    /// <summary>
-    ///     This is the channel that will be used when using the default/department prefix (<see cref="SharedChatSystem.DefaultChannelKey"/>).
-    /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public string? DefaultChannel;
 
-    /// <summary>
-    ///     Combined set of radio channels provided by all contained keys that are ReadOnly.
-    /// </summary>
+    [AutoNetworkedField]
     public HashSet<ProtoId<RadioChannelPrototype>> ReadOnlyChannels = new();
 }
